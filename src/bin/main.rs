585d4911dd983000be2151459d87a24168e3bc23
ZMC <mczhangtj@outlook.com>
2025-04-09 20:21:24 +0800

    output

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -4,7 +4,9 @@ use lib::lexer::lexer::Lexer;
 use lib::parser::grammar::Grammar;
 use lib::parser::lr1::{LR1Parser, ParseError, ReduceResult};
 use std::fs::File;
+use std::io::Write;
 use log::info;
+use lib::ast::ast_build::build_ast;
 
 fn main() -> io::Result<()>{
 
@@ -13,6 +15,7 @@ fn main() -> io::Result<()>{
     // let file_path = "testcase/array.circom";
     let file_path = "testcase/1.circom";
     let grammar_path = "grammar/grammar.txt";
+    let out_path = "out/1.json";
 
     let content = read_circom_file(file_path)?;
 
@@ -36,12 +39,23 @@ fn main() -> io::Result<()>{
 
     let parser = LR1Parser::new(grammar);
 
-    match parser.unwrap().run_parse(&tokens) {
+    let reduce_result = parser.unwrap().run_parse(&tokens);
+
+    match reduce_result {
         Ok(steps) => {
+            let s = steps.clone();
             info!("\nParse succeeded! Steps:");
             for step in steps {
                 info!("{:?}", step);
             }
+
+            let ast = build_ast(s);
+            let json_data = serde_json::to_string_pretty(&ast)?;
+            let mut file = File::create(out_path)?;
+            file.write_all(json_data.as_bytes())?;
+
+            info!("\nOutput json to: {}\n", out_path);
+
         }
         _ => {}
     }
