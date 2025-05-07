9b2d3ba947520dd88ded6bcf2a05d704e0e41f79
ZMC <1507007359@qq.com>
2025-05-08 04:09:59 +0800

    Update test_all.bat

--- test_all.bat
+++ test_all.bat
@@ -1,6 +1,7 @@
 @echo off
 for /l %%i in (1,1,23) do (
     if exist "testcase\%%i.circom" (
+        echo processing "%%i.circom"
         main.exe "testcase\%%i.circom"
     ) else (
         echo File Not Exist
