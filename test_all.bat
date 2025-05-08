3920220a3ff7c45d62912f83fe9b31b5eff6f52b
ZMC <1507007359@qq.com>
2025-05-08 18:52:38 +0800

    Update test_all.bat

--- test_all.bat
+++ test_all.bat
@@ -1,5 +1,5 @@
 @echo off
-for /l %%i in (1,1,23) do (
+for /l %%i in (1,1,30) do (
     if exist "testcase\%%i.circom" (
         echo processing "%%i.circom"
         main.exe "testcase\%%i.circom"
