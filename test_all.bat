b52b777b710d7f3387117e47045cd1e32a5b40ea
ZMC <mczhangtj@outlook.com>
2025-05-19 00:00:25 +0800

    update test.bat&sh

--- test_all.bat
+++ test_all.bat
@@ -1,9 +1,16 @@
 @echo off
+
+if exist "./target/debug/main.exe" (
+    copy /Y "./target/debug/main.exe" .
+) else (
+    echo File "main.exe" Not Exist
+)
+
 for /l %%i in (1,1,30) do (
     if exist "testcase\%%i.circom" (
         echo processing "%%i.circom"
         main.exe "testcase\%%i.circom"
     ) else (
-        echo File Not Exist
+        echo File "testcase\%%i.circom" Not Exist
     )
 )
